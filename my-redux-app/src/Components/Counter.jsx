b6c23bfd83300c5cf8774bddea41706de44008a5
supriya0003 <supriya2002arora@gmail.com>
2025-07-29 18:38:41 +0530

    counter,jsx file

--- my-redux-app/src/Components/Counter.jsx
+++ my-redux-app/src/Components/Counter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { increment, decrement } from "../Components/Counter"
+import { increment, decrement } from "../Features/CounterSlice"
 function Counter() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
