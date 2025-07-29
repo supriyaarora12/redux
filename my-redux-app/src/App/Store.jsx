c3561c32d7823b86d7c20f71ae25ed5e2e2b0f96
supriya0003 <supriya2002arora@gmail.com>
2025-07-29 18:11:37 +0530

    store file in which reducer is defined

---
+++ my-redux-app/src/App/Store.jsx
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../Features/CounterSlice';
+
+export const store = configureStore({
+  reducer: {
+    counter: counterReducer,
+  },
+});
+
